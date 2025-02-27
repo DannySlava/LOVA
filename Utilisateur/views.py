6d878dd35e690b33e1f1dc7791b98731f4b8996c
DannySlava <dannyslava0@gmail.com>
2025-02-27 18:30:11 +0300

    project one

--- Utilisateur/views.py
+++ Utilisateur/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 from django.core.exceptions import ValidationError
-from .models import Client
+from .models import Client, Professeur
 from .crypt import PasswordCrypto
 from django.contrib.auth import authenticate, login
 
-def accueil_client(request):
+def accueil(request):
     if 'id_client' not in request.session:
         return HttpResponse("Accès non autorisé")
     
-    # Récupérer les informations du client connecté
-    client_id = request.session['id_client']
-    client = Client.objects.get(id_client=client_id)
-    
-    return render(request, 'Client/accueil_client.html', {'client': client})
+    return render(request, 'Client/accueil.html')
 
 def deconnexion_client(request):
     if 'id_client' in request.session:
@@ -21,10 +17,10 @@ def deconnexion_client(request):
         return redirect('connexion_client')
     return redirect('connexion_view')
 
-def inscription_view(request):
+def inscription_client_view(request):
     return render(request, 'Client/inscription_client.html')
 
-def inscription(request):
+def inscription_client(request):
     crypto = PasswordCrypto()
     
     if request.method == 'POST':
@@ -84,7 +80,7 @@ def connexion_view(request):
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def connexion(request):
+def connexion_client(request):
     crypt = PasswordCrypto()
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -94,10 +90,69 @@ def connexion(request):
         try:
             client = Client.objects.get(email=email, mot_de_passe=mot_de_passe)
             request.session['id_client'] = client.id_client
-            return redirect('accueil_client')  # Redirige vers la page d'accueil du client
+            return redirect('accueil')  # Redirige vers la page d'accueil du client
         except Client.DoesNotExist:
             messages.error(request, "Email ou mot de passe incorrect.")
             return render(request, 'Client/connexion_client.html')
     
     # Si la méthode n'est pas POST, afficher la page de connexion
     return render(request, 'Client/connexion_client.html')
+
+def inscription_prof_view(request):
+    return render(request, 'Professeur/inscription_prof.html')
+
+def inscription_prof(request):
+    crypto = PasswordCrypto()
+    
+    if request.method == 'POST':
+        nom = request.POST.get('nom')
+        nom_majuscule = nom.upper()
+        prenom = request.POST.get('prenom')
+        date_de_naissance = request.POST.get('date_de_naissance')
+        email = request.POST.get('email')
+        mot_de_passe = request.POST.get('mot_de_passe')
+        confirm_password = request.POST.get('confirm_password')
+        numero_cin = request.POST.get('numero_cin')
+        photo_profil = request.FILES.get('photo_profil')
+        biographie = request.POST.get('biographie')
+
+        # Vérification des mots de passe
+        if mot_de_passe != confirm_password:
+            messages.error(request, "Les mots de passe ne correspondent pas.")
+            return render(request, 'Professeur/inscription_prof.html')
+
+        # Vérification si l'email ou le CIN existe déjà
+        if Professeur.objects.filter(email=email).exists():
+            messages.error(request, "Cet email est déjà utilisé.")
+            return render(request, 'Professeur/inscription_prof.html')
+
+        if Professeur.objects.filter(numero_cin=numero_cin).exists():
+            messages.error(request, "Ce numéro de CIN est déjà utilisé.")
+            return render(request, 'Professeur/inscription_prof.html')
+
+        try:
+            encrypted_password = crypto.encrypt(mot_de_passe)
+            
+            # Enregistrement du professeur
+            professeur = Professeur(
+                nom=nom_majuscule,
+                prenom=prenom,
+                date_de_naissance=date_de_naissance,
+                email=email,
+                mot_de_passe=encrypted_password,  
+                numero_cin=numero_cin,
+                photo_profil=photo_profil,
+                biographie=biographie
+            )
+            professeur.full_clean()  # Valide les contraintes du modèle
+            professeur.save()
+            
+            return redirect('connexion_view')
+            
+        except ValidationError as e:
+            messages.error(request, f"Erreur lors de l'inscription: {e}")
+        except Exception as e:
+            messages.error(request, f"Erreur lors du chiffrement du mot de passe: {str(e)}")
+
+    return render(request, 'Professeur/inscription_prof.html')
+
